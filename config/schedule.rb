fd6f68441a6b1fb8397d0d370dba41fb263d4d37
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-28 11:35:37 +0000

    Remove quotes from whenever argument.
    Whenever handles the quoting and doesn't appreciate you trying.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ set :output, {
     :standard => "#{root_path}/log/cron.out.log",
     :error => "#{root_path}/log/cron.err.log"
 }
-job_type :collector, "cd :path && RACK_ENV=:environment bundle exec collector --base_url='https://www.gov.uk' :task :output"
+job_type :collector, "cd :path && RACK_ENV=:environment bundle exec collector --base_url=https://www.gov.uk :task :output"
 
 
 
