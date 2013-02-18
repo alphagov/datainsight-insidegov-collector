54f5d6a1283f45642af5557c19a6a033b3bc52b2
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-02-18 09:25:42 +0000

    Fix collector spec by mocking API

--- spec/unit/insidegov_collector_spec.rb
+++ spec/unit/insidegov_collector_spec.rb
@@ -2,110 +2,68 @@ require "spec_helper"
 require_relative "../../lib/insidegov_collector"
 
 describe "insidegov collector" do
-  it "should return a single page of policy messages" do
-
-    policies = {
-        count: 2,
-        current_page: 1,
-        total_pages: 1,
-        total_count: 2,
-        results: [
-            {
-                id: 1,
-                type: "policy",
-                title: "Transport policy",
-                url: "/government/policies/transport-policy",
-                organisations: "<abbr title=\"Department for Communities and Local Government\">DCLG</abbr>",
-                public_timestamp: "2012-05-28T10:26:49+01:00",
-                topics: "anything"
-            },
-            {
-                id: 2,
-                type: "policy",
-                title: "Another policy",
-                url: "/government/policies/another-policy",
-                organisations: "<abbr title=\"Department for Random Stuff\">DRS</abbr>",
-                public_timestamp: "2012-12-21T23:59:59+00:00",
-                topics: "anything"
-            },
-        ]
+  before(:each) do
+    @empty_result = {:results => []}
+    @artefact = {
+      "title" => "Example title",
+      "type" => "mytype",
+      "url" => "http://example/",
+      "public_timestamp" => "2012-12-12T12:12:12",
+      "organisations" => "<abbr title=\"Department for Communities and Local Government\">DCLG</abbr>"
     }
+  end
 
-    url = "http://insidegov/government/policies.json?direction=alphabetical&page=1"
-    FakeWeb.register_uri(:get, url, body: policies.to_json)
+  it "should return policies" do
+    InsideGovApiClient.any_instance
+      .should_receive(:results)
+      .with("policies")
+      .and_return([@artefact, @artefact])
+    InsideGovApiClient.any_instance
+      .should_receive(:results)
+      .with("announcements")
+      .and_return([])
 
     collector = InsideGovCollector.new({base_url: "http://insidegov/"})
 
-    policy_iterator = collector.messages
+    messages = collector.messages.to_a
 
-    messages = policy_iterator.to_a
-
-    messages.should have(2).messages
+    messages.should have(2).items
 
     message = messages.first
     message[:envelope][:collector].should == "InsideGov"
-    message[:payload][:title].should == "Transport policy"
-    message[:payload][:url].should == "/government/policies/transport-policy"
-    message[:payload][:updated_at].should == "2012-05-28T10:26:49+01:00"
+    message[:payload][:title].should == "Example title"
+    message[:payload][:url].should == "http://example/"
+    message[:payload][:type].should == "policy"
+    message[:payload][:updated_at].should == "2012-12-12T12:12:12"
     message[:payload][:organisations].should have(1).item
     message[:payload][:organisations].first[:name].should == "Department for Communities and Local Government"
     message[:payload][:organisations].first[:abbreviation].should == "DCLG"
-
-    second_message = messages[1]
-    second_message[:envelope][:collector].should == "InsideGov"
-    second_message[:payload][:title].should == "Another policy"
-    second_message[:payload][:url].should == "/government/policies/another-policy"
-    second_message[:payload][:updated_at].should == "2012-12-21T23:59:59+00:00"
-    second_message[:payload][:organisations].should have(1).item
-    second_message[:payload][:organisations].first[:name].should == "Department for Random Stuff"
-    second_message[:payload][:organisations].first[:abbreviation].should == "DRS"
   end
 
-  it "should return two pages of policy messages" do
-    policies = {
-      count: 2,
-      current_page: 1,
-      total_pages: 2,
-      total_count: 4,
-      results: [
-        {
-          id: 1,
-          type: "policy",
-          title: "Transport policy",
-          url: "/government/policies/transport-policy",
-          organisations: "<abbr title=\"Department for Communities and Local Government\">DCLG</abbr>",
-          public_timestamp: "2012-05-28T10:26:49+01:00",
-          topics: "anything"
-        },
-        {
-          id: 2,
-          type: "policy",
-          title: "Another policy",
-          url: "/government/policies/another-policy",
-          organisations: "<abbr title=\"Department for Random Stuff\">DRS</abbr>",
-          public_timestamp: "2012-12-21T23:59:59+00:00",
-          topics: "anything"
-        },
-      ],
-      next_page: 2
-    }
+  it "should return announcements" do
+    InsideGovApiClient.any_instance
+    .should_receive(:results)
+    .with("policies")
+    .and_return([])
+    InsideGovApiClient.any_instance
+    .should_receive(:results)
+    .with("announcements")
+    .and_return([@artefact, @artefact])
 
-    url = "http://insidegov/government/policies.json?direction=alphabetical&page=1"
-    FakeWeb.register_uri(:get, url, body: policies.to_json)
+    collector = InsideGovCollector.new({base_url: "http://insidegov/"})
 
-    policies[:current_page] = 2
-    policies.delete(:next_page)
-    policies[:previous_page] = 1
+    messages = collector.messages.to_a
 
-    url = "http://insidegov/government/policies.json?direction=alphabetical&page=2"
-    FakeWeb.register_uri(:get, url, body: policies.to_json)
+    messages.should have(2).items
 
-    collector = InsideGovCollector.new(base_url: "http://insidegov/")
-
-    policy_iterator = collector.messages
-
-    messages = policy_iterator.to_a
-
-    messages.should have(4).messages
+    message = messages.first
+    message[:envelope][:collector].should == "InsideGov"
+    message[:payload][:title].should == "Example title"
+    message[:payload][:url].should == "http://example/"
+    message[:payload][:type].should == "mytype"
+    message[:payload][:updated_at].should == "2012-12-12T12:12:12"
+    message[:payload][:organisations].should have(1).item
+    message[:payload][:organisations].first[:name].should == "Department for Communities and Local Government"
+    message[:payload][:organisations].first[:abbreviation].should == "DCLG"
   end
 end
